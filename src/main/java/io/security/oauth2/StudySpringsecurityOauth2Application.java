1d046bd8d16a69fedcb6549ccd40dded773bf3f5
shin-6-0 <syk6715@naver.com>
2023-12-26 13:50:25 +0900

    initialize springSecurity Setting

---
+++ src/main/java/io/security/oauth2/StudySpringsecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package io.security.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringsecurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringsecurityOauth2Application.class, args);
+    }
+
+}
