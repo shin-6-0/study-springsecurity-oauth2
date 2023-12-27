fe872ddc5809259ae9a70124868a569e973bc1de
shin-6-0 <syk6715@naver.com>
2023-12-27 16:15:13 +0900

    [HttpBasic]HttpBasic 인증 방식 실습2 CustomAuthentication

--- src/main/java/io/security/oauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/SecurityConfig.java
@@ -11,7 +11,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-        http.httpBasic();//.authenticationEntryPoint(new CustomAuthenticationEntryPoint()); //httpBasic > 인증방식
+        http.httpBasic().authenticationEntryPoint(new CustomAuthenticationEntryPoint()); 
+        //httpBasic > 인증방식. 인증 못받았을 경우 authenticationEntryPoint로 이동
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //세션 인증 방식을 사용하지 않겠다
         return http.build();
     }
